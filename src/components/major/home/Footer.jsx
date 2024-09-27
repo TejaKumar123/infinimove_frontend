66c919ac50c8ba058a774d4d38149bb37c09fc04
TejaKumar123 <inceptioncom414@gmail.com>
2024-09-27 10:33:56 +0530

    footer modified

--- src/components/major/home/Footer.jsx
+++ src/components/major/home/Footer.jsx
@@ -99,9 +99,10 @@ const Footer = () => {
 					<div className="min-w-[30%] h-[100%] flex flex-row items-center justify-between gap-[40px] text-[white] rounded-[22px] border-[2px] border-[rgba(255,255,255,0.3)] px-[20px] py-[8px]">
 						<a className="text-[150%] font-bold">Get in Touch</a>
 						<div className="w-fit flex flex-row items-center justify-between gap-[10px]">
-							<a href="/"><img src={imageasset.Facebook} /></a>
-							<a href="/"><img src={imageasset.Linkedin} /></a>
-							<a href="/"><img src={imageasset.Instagram} /></a>
+							<a href="https://www.facebook.com/infini.move/
+" target="_blank"><img src={imageasset.Facebook} /></a>
+							<a href="https://www.linkedin.com/groups/14501329" target="_blank"><img src={imageasset.Linkedin} /></a>
+							<a href="https://www.instagram.com/invites/contact/?igsh=w27x5g1q5ch3&utm_content=vom7ob5" target="_blank"><img src={imageasset.Instagram} /></a>
 							<a href="/"><img src={imageasset.Twitter} /></a>
 						</div>
 					</div>
