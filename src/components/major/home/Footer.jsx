61ab9cb99ed86a1fb67c72c2c929135fc17e952f
TejaKumar123 <inceptioncom414@gmail.com>
2024-09-29 11:56:29 +0530

    twitter link added

--- src/components/major/home/Footer.jsx
+++ src/components/major/home/Footer.jsx
@@ -70,7 +70,7 @@ const Footer = () => {
 " target="_blank"><img src={imageasset.Facebook} /></a>
 							<a href="https://www.linkedin.com/groups/14501329" target="_blank"><img src={imageasset.Linkedin} /></a>
 							<a href="https://www.instagram.com/invites/contact/?igsh=w27x5g1q5ch3&utm_content=vom7ob5" target="_blank"><img src={imageasset.Instagram} /></a>
-							<a href="/"><img src={imageasset.Twitter} /></a>
+							<a href="https://x.com/infinimove18076"><img src={imageasset.Twitter} /></a>
 						</div>
 					</div>
 				</div>
