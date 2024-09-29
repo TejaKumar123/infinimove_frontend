ac7560ead066c3e949ab37570480f6836dac2713
TejaKumar123 <inceptioncom414@gmail.com>
2024-09-29 19:08:13 +0530

    about us page completed

--- src/assets/images/imageassets.jsx
+++ src/assets/images/imageassets.jsx
@@ -10,7 +10,16 @@ import logo1 from "./logo1.png";
 import notfound from "./notfound.jpg";
 import infinimove_logo from "./infinimove logo.png"
 import Avatar from "./Avatar.png"
+import about1 from "./about1.png"
+import about2 from "./about2.png"
+import about3 from "./about3.png"
+import Airbnb from "./company/Airbnb Logo.png"
+import FedEx from "./company/FedEx Logo.png"
+import Google from "./company/Google Logo.png"
+import Hubspot from "./company/Hubspot Logo.png"
+import Microsoft from "./company/Microsoft Logo.png"
+import Walmart from "./company/Walmart Logo.png"
 
-const imageasset = { website1, service1, service2, service3, Linkedin, Facebook, Instagram, Twitter, logo1, notfound, infinimove_logo, Avatar };
+const imageasset = { website1, service1, service2, service3, Linkedin, Facebook, Instagram, Twitter, logo1, notfound, infinimove_logo, Avatar, about1, about2, about3, Airbnb, FedEx, Google, Hubspot, Microsoft, Walmart };
 
 export default imageasset;
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,9 @@ export default {
         'footer-linear': 'linear-gradient(to top,#001FFF,#142180,#17226E,#1A225A)',
         "service-border-linear": "linear-gradient(210deg,#F5F6FF 5%,#656EB1 46%,#3749C0 100%)",
         "right-feedback-linear": "linear-gradient(to right,#001FFF25 29%,#0014A525 60%,#0B021C25 100%)",
-        "left-feedback-linear": "linear-gradient(to right,#0B021C25 29%,#0014A525 88%,#001FFF25 100%)"
+        "left-feedback-linear": "linear-gradient(to right,#0B021C25 29%,#0014A525 88%,#001FFF25 100%)",
+        "divider-linear": "linear-gradient(to right,#1A225A,#FFFFFF,#1A225A)",
+        "about-box-linear": "linear-gradient(to bottom,#8322E350,#8B76A055,#EDEDED37)"
 
       },
       fontFamily: {
